6362af67f00fe1b01a5071830d6c32f0b786ce00
crud0626 <sisofiy6048@gmail.com>
2023-01-04 14:52:45 +0900

    Styles: 글로벌스타일 및 공통 속성 값 정의

---
+++ src/styles/common/colors.ts
@@ -0,0 +1,15 @@
+// 공통
+export const mainGrayColor = "#24292F";
+export const mainBorderColor = "#D0D7DE";
+export const mainWhiteColor = "#FFFFFF"
+
+export const sectionBgColor = "#F7F8FA";
+export const todoItemHoverBorderColor = "#0969DA";
+export const iconHoverColor = "#D3D3D3";
+export const buttonBgColor = "#2DA44E";
+export const buttonHoverBgcolor = "#2C974B";
+
+// 유효성 검사 관련
+export const disableTextColor = "#808080";
+export const disableIconColor = "#D9D9D9";
+export const enableIconColor = "#F1E05A";
---
+++ src/styles/common/sizes.ts
@@ -0,0 +1,18 @@
+// 마진 & 패딩, 1x = 4px
+export const space_2x = "8px";
+export const space_4x = "16px";
+export const space_6x = "24px";
+export const space_12x = "48px";
+
+// 폰트
+export const subContentFontSize = "0.8rem";
+export const contentFontSize = "1rem";
+export const subHeaderFontSize = "1.3rem";
+export const headerFontSize = "1.5rem";
+
+// 기타
+export const detailIconSize = "20px";
+export const footerIconSize = "32px";
+export const inputBoxHeight = "45px";
+export const headerHeight = "48px";
+export const footerHeight = "48px";
