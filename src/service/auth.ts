30b63a653697d458aab8d225c5911b3419757175
crud0626 <sisofiy6048@gmail.com>
2023-01-05 11:25:32 +0900

    Feat: 로그인 관련 API추가
    * User 인터페이스 추가

---
+++ src/service/auth.ts
@@ -0,0 +1,45 @@
+import { User } from "types/User";
+
+const END_POINT = "http://localhost:8080/users";
+
+class AuthService {
+    login = async (loginData: User): Promise<string | void> => {
+        try {
+            const response = await fetch(`${END_POINT}/login`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData)
+            }).then(res => res.json());
+
+            if(response) {
+                return response.token;
+            }
+        } catch (error) {
+            throw new Error(`로그인 도중 에러가 발생했습니다. ${error}`);
+        }
+    }
+
+    signUp = async (loginData: User): Promise<string | void> => {
+        try {
+            const response = await fetch(`${END_POINT}/create`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData)
+            }).then(res => res.json());
+
+            if(response) {
+                return response.token;
+            }
+        } catch (error) {
+            throw new Error(`회원가입 도중 에러가 발생했습니다. ${error}`);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export { authService };
