1891e1af1d37a83eef977936fe93b9460d1f67c6
crud0626 <sisofiy6048@gmail.com>
2023-01-16 11:02:15 +0900

    Refator: Router 분리

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import StyledSection from './Section.styles';
-import HomePage from 'pages/HomePage';
-import SignUpPage from 'pages/SignUpPage';
-import LoginPage from 'pages/LoginPage';
+import Router from '../Router/Router';
 
 const Section = () => {
     return (
         <StyledSection>
-            <Routes>
-                <Route 
-                    path='/'
-                    element={<HomePage />}
-                />
-                <Route 
-                    path='/login'
-                    element={<LoginPage />}
-                />
-                <Route 
-                    path='/signup'
-                    element={<SignUpPage />}
-                />
-            </Routes>
+            <Router />
         </StyledSection>
     );
 };
